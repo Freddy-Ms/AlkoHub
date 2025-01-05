d7a719ce4fb1bf72eba3bbe0f0758bdb349e3c76
ArtsyKimiko <114069062+ArtsyKimiko@users.noreply.github.com>
2025-01-05 21:18:24 +0100

    corrections to the product card and homepage

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -6,7 +6,10 @@ const ProductCard = ({ product }) => {
     <div className="product-card">
       <img src={product.image_url} alt={product.name} className="product-image" />
       <h3 className="product-name">{product.nazwa_alkoholu}</h3>
-      <p className="product-description">{product.zawartosc_procentowa}</p>
+      <p className="product-description" style={{ display: 'flex', flexDirection: 'column' }}>
+        <span>Zawartość:</span>
+        <span>{product.zawartosc_procentowa}% </span>
+      </p>
     </div>
   );
 };
